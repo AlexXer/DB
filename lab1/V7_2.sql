645e7e480afe9498908d612f727e42685cfbc51f
AlexXer <alexxerman@gmail.com>
2020-10-22 14:41:34 +0300

    edit: comment in the fitst lab the second task

--- lab1/V7_2.sql
+++ lab1/V7_2.sql
@@ -17,5 +17,6 @@ SELECT TOP(5) BusinessEntityID, JobTitle, Gender, BirthDate FROM HumanResources.
 /*
 	Вывести на экран список сотрудников женского пола, принятых на работу во вторник (Tuesday). В поле [LoginID] заменить домен ‘adventure-works’ на ‘adventure-works2012’
 
+*/
 
 SELECT BusinessEntityID,JobTitle,Gender,HireDate,REPLACE(LoginID,'adventure-works', 'adventure-works2012') FROM HumanResources.Employee WHERE Gender = 'F' AND DATEPART(WEEKDAY,HireDate)='2' 
